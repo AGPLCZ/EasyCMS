5667c497a1923c2e491d79eada074d6b0e7cd4da
AGPLCZ <62847503+AGPLCZ@users.noreply.github.com>
2021-10-02 21:18:32 +0200

    new

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
     <header>
 
         <nav class="navbar navbar-expand-lg navbar-light" aria-label="Ninth navbar example" style="background-color: rgb(255, 255, 255);">
-            <div class="container-xl">
+
+            <div class="container-xl" style="margin-top: 10px; margin-bottom: 10px;">
 
 
                 <a class="navbar-brand" href="#"> <i class="fal fa-user" style="padding-right: 7px;"></i>Petr Lízal</a>
@@ -78,7 +79,7 @@
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active" style="background-image: url(img/t1.jpg);">
+                <div class="carousel-item active" style="background-image: url(img/t4.jpg);">
                     <div class="zigzag"></div>
 
                     <div class="container">
@@ -89,7 +90,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item" style="background-image: url(img/t2.jpg);">
+                <div class="carousel-item" style="background-image: url(img/t5.jpg);">
                     <div class="zigzag"></div>
 
 
@@ -125,20 +126,87 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
+
+
+
         </div>
 
 
 
 
 
+        <h1 class="visually-hidden">Heroes examples</h1>
+
+        <div class="px-4 py-5 my-5 text-center">
+
+
+            <h1 class="display-5 fw-bold">Centered hero</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                </div>
+            </div>
+        </div>
 
 
 
+        <div class="bg-light text-secondary px-4 py-5 text-center mb-5">
+            <div class="py-5">
+                <h1 class="display-5 fw-bold text-black">Dark mode hero</h1>
+                <div class="col-lg-6 mx-auto">
+                    <p class="lead mb-4 text-black"">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class=" d-grid gap-2 d-sm-flex justify-content-sm-center">
+                        <button type="button" class="btn btn-outline-secondary btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+                </div>
+            </div>
+        </div>
+        </div>
 
 
-        <!-- Marketing messaging and featurettes
-  ================================================== -->
-        <!-- Wrap the rest of the page in another container to center all the content. -->
+        <div class="bg-light  px-4 py-5 text-center mb-5 mt-5">
+
+
+            <div class="container marketing">
+
+                <!-- Three columns of text below the carousel -->
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p><i class="fas fas fa-eye" style="font-size: 45px"></i></p>
+
+                        <h2>Heading</h2>
+                        <p class="fw-light">Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                        <a class="btn btn-outline-secondary" href="#" role="button">Zobrazit &raquo;</a>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <p><i class="fas fa-comment-dots" style="font-size: 45px"></i></p>
+
+                        <h2>Heading</h2>
+                        <p class="fw-light">Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                        <a class="btn btn-outline-secondary" href="#" role="button">Zobrazit &raquo;</a>
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <p><i class="fas fa-comment-dots" style="font-size: 45px"></i></p>
+
+
+
+                        <h2>Heading</h2>
+                        <p class=" fw-light">And lastly this, the third column of representative placeholder content.</p>
+                        <a class="btn btn-outline-secondary" href="#" role="button">Zobrazit &raquo;</a>
+                    </div><!-- /.col-lg-4 -->
+                </div><!-- /.row -->
+
+                <!-- /END THE FEATURETTES -->
+
+            </div><!-- /.container -->
+
+
+
+        </div>
+
+
+
 
         <div class="container marketing">
 
@@ -243,7 +311,147 @@
 
 
 
+
+
     </main>
+
+    <main>
+
+
+
+
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="container px-4 py-5" id="icon-grid">
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+                <div class="col d-flex align-items-start">
+                    <i class="fas fas fa-eye" style="font-size: 25px; padding-right:20px;"> </i>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <i class="fas fas fa-eye" style="font-size: 25px; padding-right:20px;"> </i>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <i class="fas fas fa-eye" style="font-size: 25px; padding-right:20px;"> </i>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <i class="fas fas fa-eye" style="font-size: 25px; padding-right:20px;"> </i>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="container px-4 py-5" id="hanging-icons">
+
+
+            <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <div class="col d-flex align-items-start">
+                    <div class="icon-square text-dark flex-shrink-0 me-3">
+                        <i class="fas fas fa-handshake  rounded" style="font-size: 45px; padding: 0px;"> </i>
+
+
+                    </div>
+                    <div>
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" class="btn btn-primary">
+                            Primary button
+                        </a>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <div class="icon-square text-dark flex-shrink-0 me-3">
+                        <i class="fas fas fa-handshake  rounded" style="font-size: 45px; padding: 0px;"> </i>
+                    </div>
+                    <div>
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" class="btn btn-primary">
+                            Primary button
+                        </a>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <div class="icon-square text-dark flex-shrink-0 me-3">
+                        <i class="fas fas fa-handshake  rounded" style="font-size: 45px; padding: 0px;"> </i>
+                    </div>
+                    <div>
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <a href="#" class="btn btn-primary">
+                            Primary button
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+
+
+
+
 
 
 
