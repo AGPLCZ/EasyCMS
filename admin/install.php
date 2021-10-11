625b4f3281eef905d3cdecc7e8b0b7f8846c409c
AGPLCZ <62847503+AGPLCZ@users.noreply.github.com>
2021-10-11 23:21:42 +0200

    new

--- admin/install.php
+++ admin/install.php
@@ -120,7 +120,7 @@ FOREIGN KEY (rubrikyId) REFERENCES rubriky(id)
     if ($ok) {
         echo "Databáze vytvořena.<br>";
         echo "Počet vytvořených tabulek: " . $pocetVytvorenychTabulek . " / " . $celkemTabulekProVytvoreni;
-        header("Location: install.php?odeslano=zapsano&pocetVytvorenychTabulek=" . $pocetVytvorenychTabulek . "&celkemTabulekProVytvoreni=" . $celkemTabulekProVytvoreni);
+        header("Location: userRegistrace.php?odeslano=dbok&pocetVytvorenychTabulek=" . $pocetVytvorenychTabulek . "&celkemTabulekProVytvoreni=" . $celkemTabulekProVytvoreni);
         exit();
     } else {
         echo "Nastala neznámá chyba!";
@@ -207,12 +207,12 @@ FOREIGN KEY (rubrikyId) REFERENCES rubriky(id)
                                                     if (isset($_GET["odeslano"])) {
 
                                                         // Hláška z GET - účet vytvořen
-                                                        if ($_GET["odeslano"] == "zapsano") {
+                                                        if ($_GET["odeslano"] == "dbok") {
                                                             echo '<p class="btn btn-warning">
 													
 															<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>
 															
-															Účet byl vytvořen</p>';
+															Databáze vytvořena</p>';
                                                         }
                                                     }
 
